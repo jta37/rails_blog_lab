230ffbfec3754abc94ecd0c4b2594a48033bbc33
Tim Licata <tim@timothylicata.com>
2014-08-21 15:40:29 -0700

    add nested resource routing for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   get 'tags', to: 'tags#index'
   get 'tags/:id', to: 'tags#show'
