5fb0ab94f5a7798631efa10ded3ffad86a9fa4f9
Tim Licata <tim@timothylicata.com>
2014-08-21 16:06:39 -0700

    define comments#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,11 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
   end
+  def create
+    comment_params = params.require(:comment).permit(:title, :body)
+    post = Post.find(params[:post_id])
+    comment = post.comments.create(comment_params)
+    redirect_to [post, comment]
+    # redirect_to post_comment_path(post, comment)
+  end
 end
