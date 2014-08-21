5bafba9df142d4b59a15b4cfc30e9581e968f1f1
Tim Licata <tim@timothylicata.com>
2014-08-21 15:29:06 -0700

    use "resources :posts" in place of manually created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
-  get 'posts/:id/edit', to: 'posts#edit'
+  resources :posts
+
   get 'tags', to: 'tags#index'
   get 'tags/:id', to: 'tags#show'
-
-  post 'posts/create', to: 'posts#create'
-  delete 'posts/:id', to: 'posts#delete'
-  put 'posts/:id', to: 'posts#update'
 end
