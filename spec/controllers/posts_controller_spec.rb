aefcd9f3ec0922050a5368efc412703a2d7eabe9
Tim Licata <tim@timothylicata.com>
2014-08-22 08:51:20 -0700

    test that home page returns successful HTTP 200 status

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
-
+  describe "GET #index" do
+    it "responds successfully with HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
